af62835a50efe0117943bec692e417c4307099af
KapoorKartik <87421857+KapoorKartik@users.noreply.github.com>
2021-11-29 14:51:10 +0530

    commit -1

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const companySchema = new mongoose.Schema(
     {
     company_name : {type : String , require : true },
     establish : {type : Number , require : true},
-    head_ofiice : {type : String , require : true },
+    head_office : {type : String , require : true },
     },{
     versionKey : false,
     timestamps : true 
@@ -21,14 +21,42 @@ const companySchema = new mongoose.Schema(
 
 const Company = mongoose.model("company",companySchema)
 
+
+/*-------------job Schema-----------------*/
 const jobSchema = new mongoose.Schema(
     {
+    
     },{
     versionKey : false,
     timestamps : true 
     }
     
 )
+
+/*----------- company CRUD -----------*/
+app.post('/companies', async (req,res) => {
+    try{
+    const company = await Company.create(req.body)
+    
+    return res.send(company)
+    }catch(e){
+     res.status(500).json({message : e.message})
+      }
+
+  })
+
+app.get('/companies', async (req,res) => {
+    try{
+    const company = await Company.find().lean().exec()
+    
+    return res.send(company)
+    }catch(e){
+     res.status(500).json({message : e.message})
+      }
+
+  })
+
+
 
 app.listen(2000,  async()=>{
     await connect()
