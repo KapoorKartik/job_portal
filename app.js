36ccb8e0289a555dd14379526f07aa46e5d14609
KapoorKartik <87421857+KapoorKartik@users.noreply.github.com>
2021-11-29 14:30:45 +0530

    cretating schema

--- app.js
+++ app.js
@@ -8,6 +8,28 @@ const connect = () => {
     return mongoose.connect("mongodb+srv://kapoorkartik:2jhAiBXJCU26Lg4@cluster0.lkd0p.mongodb.net/job_portal?retryWrites=true&w=majority")
 }
 
+const companySchema = new mongoose.Schema(
+    {
+    company_name : {type : String , require : true },
+    establish : {type : Number , require : true},
+    head_ofiice : {type : String , require : true },
+    },{
+    versionKey : false,
+    timestamps : true 
+    }
+)
+
+const Company = mongoose.model("company",companySchema)
+
+const jobSchema = new mongoose.Schema(
+    {
+    },{
+    versionKey : false,
+    timestamps : true 
+    }
+    
+)
+
 app.listen(2000,  async()=>{
     await connect()
     console.log('listening to port 2000')
